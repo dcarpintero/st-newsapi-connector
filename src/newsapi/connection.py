f215515697dd91d92075e96ae11b12ff17f40c29
Diego Carpintero <6709785+dcarpintero@users.noreply.github.com>
2023-08-14 17:45:53 +0200

    add requests.Session return to cursor

--- src/newsapi/connection.py
+++ src/newsapi/connection.py
@@ -17,7 +17,7 @@ class NewsAPIConnection(ExperimentalBaseConnection[requests.session]):
               https://newsapi.org/docs 
     """
 
-    def _connect(self) -> requests.session:
+    def _connect(self, **kwargs) -> requests.session:
         """
         Initializes the connection parameters and creates a persistent requests.Session for connecting with the NewsAPI.
         The Session object uses an HTTPAdapter to allow for maximum retries in case of network issues.
@@ -40,7 +40,7 @@ class NewsAPIConnection(ExperimentalBaseConnection[requests.session]):
         self.session.mount("https://", HTTPAdapter(max_retries=self.retries))
         return self.session
 
-    def cursor(self):
+    def cursor(self) -> requests.Session:
         """
         Gets the current session object, creating a new one if it doesn't exist.
 
